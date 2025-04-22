a7b666f7976b864f67a35cd6b441754684fc60f1
Amir <mfathulqorib97@gmail.com>
2025-04-22 14:03:33 +0700

    update query view

--- documents/urls.py
+++ documents/urls.py
@@ -3,5 +3,5 @@ from .views import DocumentUploadView, QueryView
 
 urlpatterns = [
     path('documents/', DocumentUploadView.as_view(), name="documents"),
-    path('query/', QueryView.as_view(), name="query"),
+    path('query/<str:id>/', QueryView.as_view(), name="query"),
 ] 
--- documents/views.py
+++ documents/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from .models import Document
 from .task import process_document
@@ -21,19 +21,61 @@ class DocumentUploadView(View):
 
         return redirect('documents')
     
-class QueryView(View):
-    def get(self, request):
-        return render(request, template_name='documents/query.html')
+# class QueryView(View):
+#     template_name = 'documents/query.html'
+#     pk_url_kwarg = 'id'
+
+#     def get_object(self):
+#         return get_object_or_404(klass=Document, pk=self.kwargs.get(self.pk_url_kwarg))
+
+#     def get(self, request, *args, **kwargs):
+#         delivery = self.get_object()
+
+#         return render(request, template_name='documents/query.html', context={"delivery": delivery})
     
-    def post(self, request):
+#     def post(self, request, *args, **kwargs):
+#         delivery = self.get_object()
+#         collection_name = delivery.id
+
+#         print("collection name: ", collection_name)
+
+#         query = request.POST.get("query")
+
+#         collection = chroma.get_collection(name=collection_name, embedding_function=openai_ef)
+#         result = collection.query(
+#             query_texts=[query],
+#             n_results=3,
+#         )
+
+#         print(result)
+        
+#         return redirect('query')
+    
+class QueryView(View):
+    template_name = 'documents/query.html'
+    pk_url_kwarg = 'id'
+
+    def get_object(self):
+        return get_object_or_404(Document, pk=self.kwargs.get(self.pk_url_kwarg))
+
+    def get(self, request, *args, **kwargs):
+        delivery = self.get_object()
+        return render(request, self.template_name, context={"delivery": delivery})
+    
+    def post(self, request, *args, **kwargs):
+        delivery = self.get_object()
+        collection_name = delivery.id
+
+        print("collection name:", collection_name)
+
         query = request.POST.get("query")
 
-        collection = chroma.get_collection(name="6807110bd2aa9f74e5d3bc33", embedding_function=openai_ef)
+        collection = chroma.get_collection(name=collection_name, embedding_function=openai_ef)
         result = collection.query(
             query_texts=[query],
             n_results=3,
         )
 
         print(result)
-        
-        return redirect('query')
+
+        return redirect('query', id=delivery.id)
